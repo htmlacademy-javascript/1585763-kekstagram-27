0d6b0541e62098143bb16fd5ad52fd8fc049cd37
Keks <keks@htmlacademy.ru>
2022-10-05 09:05:13 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,21 +1,18 @@
 function generatorNumber(numberOne, numberTwo) {
-  if ((numberOne >= 0 && numberTwo >= 0) && (numberOne !== numberTwo) && (numberOne < numberTwo)) {
+  if ((numberOne >= 0 && numberTwo >= 0) && (numberOne !== numberTwo && numberOne < numberTwo)) {
     return Math.floor(Math.random() * (numberTwo - numberOne + 1)) + numberOne;
-  } else if ((numberOne !== Number || numberOne < 0) || (numberTwo !== Number || numberTwo < 0)) {
-    return NaN;
-  } else if (numberOne >= numberTwo) {
+  }
+  if ((numberOne < 0 || numberTwo < 0) || (numberOne >= numberTwo)) {
     return NaN;
   }
 }
 
+// eslint-disable-next-line no-console
 console.log(generatorNumber(1, 100));
 
 function rowLimit(string, number) {
-  if (string.length <= number) {
-    return true;
-  } else {
-    return false;
-  }
+  return string.length <= number;
 }
 
+// eslint-disable-next-line no-console
 console.log(rowLimit('Привет', 2));
