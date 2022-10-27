235e2d52bdd7f4dabf54b58da390cbc055a9aa6a
Sofia <s.bolesta@yandex.ru>
2022-10-27 18:48:54 +0700

    отрисуй меня полностью (часть 2)

--- js/render-posts.js
+++ js/render-posts.js
@@ -5,14 +5,12 @@ const postsContainers = document.querySelector('.pictures');
 const postsTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const postsFragment = document.createDocumentFragment();
 
-randomPosts.forEach(({url, description, likes}) => {
+randomPosts.forEach(({url, comments, likes}) => {
   const postsElement = postsTemplate.cloneNode(true);
   postsElement.querySelector('.picture__img').src = url;
-  postsElement.querySelector('.picture__comments').textContent = description;
+  postsElement.querySelector('.picture__comments').textContent = comments;
   postsElement.querySelector('.picture__likes').textContent = likes;
   postsFragment.append(postsElement);
 });
 
 postsContainers.append(postsFragment);
-
-console.log(postsFragment);
